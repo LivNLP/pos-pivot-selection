90326bc196a00fcccb546cb6a778a4803a7cac35
summer1278 <cxcui188@gmail.com>
2017-01-13 18:33:44 +0000

    split

--- src/pos_data.py
+++ src/pos_data.py
@@ -1,14 +1,22 @@
 import numpy as np
 import re
+import itertools
 
+# collect words and pos tags for experiments
 def collect(domain):
     input_file = open("../data/gweb-%s-dev.conll"%domain,'r')
+    words = []
     for line in input_file:
         p = filter(None,re.split("\t",line))
-        print p
+        words.append(p)
+    print split_on_sentences(words)
     pass
 
-
+# a method to group words into sentences by new lines
+def split_on_sentences(lst):
+    w=["\n"]
+    spl = [list(y) for x, y in itertools.groupby(lst, lambda z: z == w) if not x]
+    return spl
 
 if __name__ == "__main__":
     domain = "answers"
