177b949651024f7c88371d2594dcf6a10b3d8f1b
summer1278 <cxcui188@gmail.com>
2017-05-28 20:57:44 +0100

    fix tab error

--- src/pivot_wordnet.py
+++ src/pivot_wordnet.py
@@ -14,32 +14,32 @@ def count_nouns(selected_pivots):
     return float(sum(1 for word in selected_pivots if is_noun(word)==1))/float(len(pivots))
 
 def runner(source,target,method,n):
-	features = pos_data.load_obj(source,target,method) if 'landmark' not in method else pos_data.load_obj(source,target,'/test/'+method)
+    features = pos_data.load_obj(source,target,method) if 'landmark' not in method else pos_data.load_obj(source,target,'/test/'+method)
     pivots = dict(features[:n]).keys()
     print count_nouns(pivots)
     pass
 
 # different methods
 def batch_results_from_methods(source,target,methods,n):
-	print "source = ", source
-	print "target = ", target
-	for method in methods:
-		print "method = ", method
-		runner(source,target,method,n)
-	pass
+    print "source = ", source
+    print "target = ", target
+    for method in methods:
+        print "method = ", method
+        runner(source,target,method,n)
+    pass
 
 # different number of pivots
 def batch_reuslts_from_numbers(source,target,method,nums):
-	print "source = ", source
-	print "target = ", target
-	for n in nums:
-		print "#pivots = ", n
-		runner(source,target,method,n)	
-	pass
+    print "source = ", source
+    print "target = ", target
+    for n in nums:
+        print "#pivots = ", n
+        runner(source,target,method,n)    
+    pass
 
 if __name__ == '__main__':
-	source = "wsj"
-	target = "answers"
-	method = "freq"
-	n = 500
-	runner(source,target,method,n)
+    source = "wsj"
+    target = "answers"
+    method = "freq"
+    n = 500
+    runner(source,target,method,n)
