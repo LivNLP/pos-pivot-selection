4fd3aa0aa124be815c165b7a6d0b0d487a94b940
summer1278 <cxcui188@gmail.com>
2017-05-28 07:52:51 +0100

    update wordnet

--- src/pivot_wordnet.py
+++ src/pivot_wordnet.py
@@ -10,7 +10,15 @@ def is_noun(word):
 def count_nouns(selected_pivots):
     return float(sum(1 for word in selected_pivots if is_noun(word)==1))/float(len(pivots))
 
-def runner():
+def runner(source,target,method,n):
+	features = pos_data.load_obj(source,target,method) if 'landmark' not in method else pos_data.load_obj(source,target,'/test/'+method)
+    pivots = dict(features[:n]).keys()
+    print count_nouns(pivots)
     pass
 
 if __name__ == '__main__':
+	source = "wsj"
+	target = "answers"
+	method = "freq"
+	n = 500
+	runner(source,target,method,n)
