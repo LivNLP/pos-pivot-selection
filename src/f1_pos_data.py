98b9bc521cc8619e32a0ba7eda63f97839affcb9
summer1278 <cxcui188@gmail.com>
2017-06-08 03:19:10 +0100

    add classifier

--- src/f1_pos_data.py
+++ src/f1_pos_data.py
@@ -15,6 +15,7 @@ from sklearn.externals.joblib import Memory
 from sklearn.datasets import load_svmlight_file
 from sklearn.model_selection import cross_val_score,cross_val_predict
 from sklearn.metrics import f1_score
+from sklearn.linear_model import LogisticRegressionCV
 
 # add f1 when sum up the scores, using test data and results obtained from model
 def sum_up_f1_labeled_scores_test(source,target,opt):
@@ -165,6 +166,7 @@ def load(source,target):
     X, y = data[0], data[1]
     print X.shape
     print y.shape
+    clf = LogisticRegressionCV(solver='liblinear').fit(X,y)
     scores = cross_val_score(clf, X, y, cv=5, scoring='f1_macro')
     predicted = cross_val_predict(clf, X, y, cv=5)
     print scores
