0cd52584896516d666093fe23e91607fae4c81ee
summer1278 <cxcui188@gmail.com>
2017-06-09 13:02:16 +0100

    self compute f1

--- src/f1_pos_data.py
+++ src/f1_pos_data.py
@@ -10,12 +10,14 @@ import os,subprocess
 import pickle
 import test_eval
 import numpy
+import random
 
 from sklearn.externals.joblib import Memory
 from sklearn.datasets import load_svmlight_file
 from sklearn.model_selection import cross_val_score,cross_val_predict
 from sklearn.metrics import f1_score,classification_report
 from sklearn.linear_model import LogisticRegressionCV
+
 
 # add f1 when sum up the scores, using test data and results obtained from model
 def sum_up_f1_labeled_scores_test(source,target,opt):
@@ -80,10 +82,7 @@ def multiply_f1(L,f1):
     return dict([(x,f1*v) for (x,v) in L.iteritems()])
 
 def sum_up_f1_labeled_scores(source,target,opt):
-    
-    # test_file = 
-    # testLBFGS(test_file,model_file)
-    
+
     # n_splits = 5
     # original_data = read_file(original_train)
     # det_idx = int(len(original_data)/n_splits)
@@ -93,37 +92,79 @@ def sum_up_f1_labeled_scores(source,target,opt):
     # new_train = random_data[det_idx:]
     # new_test = random_data[:det_idx]
     
+    tags,f1s = compute_f1(source,target,opt)
+    methods = ['freq','mi','pmi','ppmi']
+    for method in methods:
+        for idx,pos_tag in enumerate(tags):
+            # print "TAG = %s"% pos_tag
+            f1 = f1s[idx]
+            # print "f1 = %f" % f1
+            if method == 'freq':
+                # print "FREQ-L"
+                tmp = pos_data.select_pivots_freq_labeled_tag(source,target,pos_tag)
+                freq_dict = pos_data.combine_dicts(freq_dict,multiply_f1(tmp,f1))
+            elif method == 'mi':
+                # print "MI-L"
+                tmp = pos_data.select_pivots_mi_labeled_tag(source,target,pos_tag)
+                mi_dict = pos_data.combine_dicts(mi_dict,multiply_f1(tmp,f1))           
+            elif method == 'pmi':
+                # print "PMI-L"
+                tmp = pos_data.select_pivots_pmi_labeled_tag(source,target,pos_tag)
+                pmi_dict = pos_data.combine_dicts(pmi_dict,multiply_f1(tmp,f1))           
+            else:
+                # print "PPMI-L"
+                tmp = pos_data.select_pivots_ppmi_labeled_tag(source,target,pos_tag)
+                ppmi_dict = pos_data.combine_dicts(ppmi_dict,multiply_f1(tmp,f1))
+    freq_list = freq_dict.items()
+    mi_list = mi_dict.items()
+    pmi_list = pmi_dict.items()
+    ppmi_list = ppmi_dict.items()
 
+    freq_list.sort(lambda x, y: -1 if x[1] > y[1] else 1)
+    mi_list.sort(lambda x, y: -1 if x[1] > y[1] else 1)
+    pmi_list.sort(lambda x, y: -1 if x[1] > y[1] else 1)
+    ppmi_list.sort(lambda x, y: -1 if x[1] > y[1] else 1)
+
+    print "FREQ", freq_list[:10]
+    print "MI", mi_list[:10]
+    print "PMI", pmi_list[:10]
+    print "PPMI", ppmi_list[:10]
+    save_f1_obj(source,target,freq_list,"%s/freq"%opt)
+    save_f1_obj(source,target,mi_list,"%s/mi"%opt)
+    save_f1_obj(source,target,pmi_list,"%s/pmi"%opt)
+    save_f1_obj(source,target,ppmi_list,"%s/ppmi"%opt)
+
+
+    pass
+
+def compute_f1(source,target,opt):
+    train_file = '../work/%s-%s/trainVects.NA' % (source,target)
+    model_file = '../work/%s-%s/model.NA' % (source, target)
+    test_file = '../work/%s-%s/self_testVects.NA' % (source,target)
+    generate_test_file(train_file,test_file)
+    testLBFGS(test_file,model_file)
+    output = "../work/%s-%s/f1/output"
+    predict_labels = test_eval.read_labels(output)
+    target_labels = test_eval.read_labels(test_file)
     tag_list = test_eval.generate_tag_list(source,target)
     tag_dist = pos_data.compute_dist(source)
-    res_list = compare_labels(predict_labels,target_labels,tag_list,tag_dist)
+    res_list = test_eval.compare_labels(predict_labels,target_labels,tag_list,tag_dist)
     # order might be different, so generate a tag_list
     tags = [x[0] for x in res_list]
     print tags==tag_list
     f1s = [x[4] for x in res_list] if opt=='r' else [x[6] for x in res_list]
+    print f1s
+    return tags,f1s
 
+def generate_test_file(input_fname, output_fname):
+    input_file = open(input_fname,'r')
+    lines = input_file.readlines()
+    print "train data: ",len(lines)
+    new_lines = random.sample(lines,len(lines)/5)
+    print "random select: ",len(new_lines)
+    output_file = open(output_fname,'w')
+    output_file.writelines(new_lines)
     pass
-
-
-def train_cv(source,target):
-    train_file = '../work/%s-%s/trainVects.NA' % (source,target)
-    model_file = '../work/%s-%s/model5fold.NA' % (source,target)
-    print "Training...5-fold cross-validation..."
-    train5fold(train_file,model_file)
-    pass
-
-# def read_file(fname):
-#     input_file = open(fname,'r')
-#     lines = input_file.readlines()
-#     print len(lines)
-#     return lines
-
-# 5-fold cross-validation
-def train5fold(train_file, model_file):
-    retcode = subprocess.call('~/liblinear-multicore-2.11-1/train -s 0 -v 5 -n 8 %s %s' %\
-        (train_file,model_file), shell=True)
-    #> /dev/null
-    return retcode
 
 
 def testLBFGS(test_file, model_file):
@@ -184,8 +225,6 @@ def load(source,target):
 if __name__ == '__main__':
     source = 'wsj'
     target = 'answers'
-    # sum_up_f1_labeled_scores(source,target,'r')
+    sum_up_f1_labeled_scores(source,target,'r')
     # sum_up_f1_labeled_scores(source,target,'w')
-    # read_file('../work/%s-%s/trainVects.NA'%(source,target))
-    # train_cv(source,target)
-    load(source,target)
+    # load(source,target)
