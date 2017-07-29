ed9ae7b4a9568e95430bc216ed2c930af5fa178b
summer1278 <cxcui188@gmail.com>
2017-07-29 22:42:49 +0100

    update

--- src/pos_data.py
+++ src/pos_data.py
@@ -108,10 +108,9 @@ def compute_dist(source):
     #     f.write('%s %f\n'%(x,v))
     # f.close()
     # print L
-    D = L
     plt.figure(figsize=(12,5.5))
-    x =[x for (x,v) in L]
-    y =[v for (x,v) in D]
+    x =L.keys()
+    y =L.values()
     # print x
     # print y
     index = np.arange(len(x))
