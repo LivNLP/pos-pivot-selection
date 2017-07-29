40553fddd992943336e02fcdca1e1aa660dbe4fa
summer1278 <cxcui188@gmail.com>
2017-07-29 22:56:33 +0100

    regenreate

--- src/test_eval.py
+++ src/test_eval.py
@@ -479,7 +479,7 @@ def print_gamma_results():
     pass
 
 if __name__ == '__main__':
-    print_results()
+    # print_results()
     # print_f1_results()
     # print_gamma_results()
     # test_sort()
