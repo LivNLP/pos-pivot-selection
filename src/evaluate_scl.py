c04579976ba6a43bc6bd56e5ae9f77dec147a654
summer1278 <cxcui188@gmail.com>
2017-07-31 20:05:20 +0100

    change one label

--- src/evaluate_scl.py
+++ src/evaluate_scl.py
@@ -1088,11 +1088,12 @@ if __name__ == '__main__':
     # methods = ['pmi','un_pmi','freq','un_freq','mi','un_mi','ppmi','un_ppmi']
     # methods += ['landmark_pretrained_word2vec','landmark_pretrained_word2vec_ppmi','landmark_pretrained_glove','landmark_pretrained_glove_ppmi']
     # methods = ['landmark_pretrained_word2vec','landmark_pretrained_glove']
-    methods = ['un_freq','un_mi','un_pmi','un_ppmi']
+    methods = ['un_freq','un_mi']
+    #,'un_pmi','un_ppmi'
     for method in methods:
         # pos_tag = 'NN'
         # method='%s.%s'%(method,pos_tag)
-        evaluate_POS(source, target, True, 100 ,method, n)
+        evaluate_POS(source, target, True, 1 ,method, n)
         # batchEval(method, 1, n)
         # batchEval_one_domain_pair(source,target,method,1,n)
         # batchEval_lexical(method, 1, n)
